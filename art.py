d71648e2a6bc6a5827929715e33a68e3a7841f17
Shiva <shiv@shiv.me>
2012-10-17 21:39:04 -0700

    init commit

---
+++ art.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+
+import sys
+from pyfiglet import Figlet
+from optparse import OptionParser
+
+def draw(f, font, text): 
+    f.setFont(font=font)
+    print f.renderText(text)
+
+def main(args):
+    parser = OptionParser()
+    parser.add_option("-f", "--font", help="specify the font", 
+                action="store", type="string", dest="font", default='clr8x8')
+    parser.add_option("-l", "--list", help="list available fonts", 
+                action="store_true", dest="listfonts", default=False)
+
+    (options, args) = parser.parse_args()
+    
+    print "" 
+
+    f = Figlet()
+    if options.listfonts:
+        for font in f.getFonts():
+            print font
+    else:   
+        draw(f, options.font, 'Thanks Levi & Andras')
+        draw(f, options.font, 'for the cards !!!') 
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv[1:]))
+
