d55042213db361c045fb79ab341aae624e3c8f72
Shivanand Velmurugan <shivanand.velmurugan@ericsson.com>
2012-10-19 15:39:11 -0700

    proper python setup script and files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='asciiart',
+    version='0.0.2',
+    author='Shiva Velmurugan',
+    author_email='shiv@shiv.me',
+    scripts=['bin/art.py'],
+    url='https://github.com/shiva/asciiart'
+    license='LICENSE.md',
+    description='Generate Ascii Art using pyfiglet.',
+    long_description=open('README.md').read(),
+    install_requires=['pyfiglet >= 0.6.1'],
+    dependency_links = [
+        "https://github.com/pwaller/pyfiglet.git"
+    ],
+)
+
